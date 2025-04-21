09e3f1fe38e10c5ce82c3e0871b1cce1cf4c798d
moaidshurafa <moayada124@gmail.com>
2025-04-22 00:15:10 +0300

    Add project files.

---
+++ Tarmiz/Tarmiz.API/Controllers/ListingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarmiz.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListingController : ControllerBase
+    {
+
+    }
+}
